f5dee171291bc37bd5575778476ef2f5958bb5ef
Memphis <matheus.meneguim@gmail.com>
2025-01-12 23:42:40 -0300

    feat: mapa mental adicionado, 1 imagem

---
+++ mapa-mental/Cloud Computing.mm
@@ -0,0 +1,38 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1736732148729" ID="ID_131908320" MODIFIED="1736732178975" TEXT="Cloud Computing">
+<font BOLD="true" NAME="SansSerif" SIZE="24"/>
+<node CREATED="1736732196859" ID="ID_1488397947" MODIFIED="1736732198727" POSITION="right" TEXT="">
+<icon BUILTIN="idea"/>
+<node COLOR="#000000" CREATED="1736732336414" ID="ID_1654253780" MODIFIED="1736732363659" TEXT="internet">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="password"/>
+<node COLOR="#000000" CREATED="1736732199432" ID="ID_535540152" MODIFIED="1736732313329" TEXT="conjunto">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1736732263485" ID="ID_304410619" MODIFIED="1736732263485" TEXT="aplica&#xe7;&#xf5;es"/>
+<node CREATED="1736732277299" ID="ID_1522769925" MODIFIED="1736732279247" TEXT="armazenamento"/>
+<node CREATED="1736732287860" ID="ID_263803632" MODIFIED="1736732290496" TEXT="computa&#xe7;&#xe3;o"/>
+</node>
+</node>
+<node CREATED="1736732511835" FOLDED="true" ID="ID_1828166807" MODIFIED="1736732949408" TEXT="">
+<icon BUILTIN="bell"/>
+<node CREATED="1736732921720" ID="ID_1861412346" MODIFIED="1736732937116">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="o-que-e-cloud-computing-nuvem.jpg" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1736733513203" ID="ID_1124062243" MODIFIED="1736733516431" TEXT="2">
+<icon BUILTIN="bell"/>
+<node CREATED="1736733517117" ID="ID_1143161862" MODIFIED="1736733521280" TEXT=" armazenamento de dados"/>
+<node CREATED="1736733552461" ID="ID_773175704" MODIFIED="1736733552944" TEXT="compartilhamento de arquivos"/>
+<node CREATED="1736735588537" ID="ID_1892902235" MODIFIED="1736735588537" TEXT=""/>
+</node>
+</node>
+</node>
+</map>
